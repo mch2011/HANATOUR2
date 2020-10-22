c1ec8bf3e2240be0761b3ae41634efff54a078fe
unknown <HB816@HNT.DOMAIN>
2020-10-22 18:20:15 +0900

    Initial commit

---
+++ SCLearn/P_dataset.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import load_iris
+
+iris_data = load_iris()
+keys = iris_data.keys()
+print('IRIS''s Keys', keys)
+
+#Result
+#IRISs Keys dict_keys(['data', 'target', 'frame', 'target_names', 'DESCR', 'feature_names', 'filename'])
+#
+
+print('IRIS''s Keys', keys)
+
+print('feature names''s shape:', len(iris_data.feature_names))
+print('feature names :', iris_data.feature_names)
+
+print('target names''s shape:', len(iris_data.target_names))
+print('target names :', iris_data.target_names)
+
+print('data shape :', iris_data.data.shape)
+print('data :', iris_data['data'])
+
+print('target shape :', iris_data.target.shape)
+print('target :', iris_data.target)
